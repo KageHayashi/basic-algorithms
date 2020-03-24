1a8570d57d8d07612fd8332bb00de03344ff0811
kagehayash <kagehayash@gmail.com>
2020-03-23 21:07:26 -0700

    comments added

--- algorithms/fibonacciNumbers.py
+++ algorithms/fibonacciNumbers.py
@@ -5,6 +5,7 @@ def fibonacciNumbers(n):
         fibonacci_num.append(fibonacci_num[i] + fibonacci_num[i+1])
     return fibonacci_num
 
+### Returns the first N terms of the even fibonacci numbers
 def evenFibonacciNumbers(n):
     if n < 2:
         return(0)
@@ -19,6 +20,8 @@ def evenFibonacciNumbers(n):
 
         if ef3 > n:
             break
+
+        # update to next even values and update list
         ef1 = ef2
         ef2 = ef3
         even_fibonacci_num.append(ef2)
--- algorithms/primeFactors.py
+++ algorithms/primeFactors.py
@@ -5,15 +5,21 @@ import math
 def primeFactors(n):
     prime_factors = []
 
+    # divide all the 2's out first
     while n % 2 == 0:
         prime_factors.append(2)
         n = n / 2
 
+    # now n should be an odd number so we iterate at 2
+    # and find all the numbers less than sqrt(n)
+    # that can be evenly divide n
     for i in range(3, int(math.sqrt(n)), 2):
         while n % i == 0:
             prime_factors.append(int(i))
             n = n / i
 
+    # at the end, if n is a prime and greater than 2
+    # add n into the list
     if n > 2:
         prime_factors.append(int(n))
 
