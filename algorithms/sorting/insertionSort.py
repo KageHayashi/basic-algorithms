2c5eff43b78a24316f8a51431d86e7c233b68119
kagehayash <kagehayash@gmail.com>
2020-03-25 12:35:09 -0700

    pepe

--- algorithms/sorting/insertionSort.py
+++ algorithms/sorting/insertionSort.py
@@ -7,16 +7,16 @@
 # Worst: O(1)
 
 def insertionSort(a):
-	for i in range(1, len(a)):
-		key = a[i]
+    for i in range(1, len(a)):
+        key = a[i]
 
-		j = i - 1
-		while j >= 0 and key < a[j]:
-			a[j + 1] = a[j]
-			j -= 1
-		a[j + 1] = key
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
 
 test = [2, 9, 7, 5, 8, 3, 1, 5, 0, 6, 4]
 insertionSort(test)
 for i in test:
-	print(i)
+    print(i)
--- algorithms/sorting/selectionSort.py
+++ algorithms/sorting/selectionSort.py
@@ -1,7 +1,7 @@
 # Time Complexity:
-# Best: Ω(n^2)	
-# Average: Θ(n^2)	
-# Worst: O(n^2)	
+# Best: Ω(n^2)  
+# Average: Θ(n^2)   
+# Worst: O(n^2) 
 # 
 #Space Complexity:
 # Worst: O(1)
@@ -9,15 +9,15 @@
 
 
 def selectionSort(a):
-	for i in range(len(a)):
-		min_index = i
-		for j in range(i + 1, len(a)):
-			if a[min_index] > a[j]:
-				min_index = j
+    for i in range(len(a)):
+        min_index = i
+        for j in range(i + 1, len(a)):
+            if a[min_index] > a[j]:
+                min_index = j
 
-		a[i], a[min_index] = a[min_index], a[i]
+        a[i], a[min_index] = a[min_index], a[i]
 
 test = [2, 9, 7, 5, 8, 3, 1, 5, 0, 6, 4]
 selectionSort(test)
 for i in test:
-	print(i)
+    print(i)
